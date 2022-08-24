a325341ce9590dcb50f65bdb55187eb3aad581c2
huseyindogan00 <huseyindgn00@gmail.com>
2022-08-24 19:58:20 +0300

    registerpage stabil

--- lib/view_model/register_view_model.dart
+++ lib/view_model/register_view_model.dart
@@ -1,8 +1,9 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 import 'package:jista/model/user_model.dart';
 import 'package:jista/service/firebase_service.dart';
-import 'package:jista/utility/internet_connection_status.dart';
+import 'package:jista/utility/internet_connection_control.dart';
 
 class RegisterViewModel {
   static final FirebaseService _firebaseService = FirebaseService();
@@ -12,15 +13,23 @@ class RegisterViewModel {
     return await _firebaseService.save(userModel, context);
   }
 
-  static Future<bool> internetControl() async {
-    InternetConnectionStatus connection = InternetConnectionStatus();
-    ConnectivityResult status = await connection.connectionStatus();
-    print('*********************internetControl değeri : $status');
+  // CİHAZI İNTERNETE BAĞLAYAN UÇLARI AÇIK OLSA DAHİ İNTERNET
+  // BAĞLANTISININ SAĞLANIP SAĞLANMADIĞINI KONTROL EDER
+  static Future<bool> isDeviceConnected() async {
+    bool isDeviceConnected = await InternetConnectionChecker().hasConnection;
+    return isDeviceConnected;
+  }
 
-    if (status == ConnectivityResult.none) {
-      return false;
-    } else {
-      return true;
+  // BU METHOD CİHAZI İNTERNETE BAĞLAYAN WİFİ, MOBİL VERİ VS OLAN UÇLARINI KONTROL EDER
+  static Future<bool> internetControl() async {
+    bool hasDeviceConnected = await isDeviceConnected();
+    if (hasDeviceConnected) {
+      InternetConnectionControl connection = InternetConnectionControl();
+      ConnectivityResult status = await connection.connectionStatus();
+      if (status != ConnectivityResult.none) {
+        return true;
+      }
     }
+    return false;
   }
 }
