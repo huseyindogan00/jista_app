1a053a229f2abcc1f844befdee772b9808616135
HuseyinDOGAN <huseyindgn00@gmail.com>
2022-09-25 23:17:29 +0300

    category edit, hatalı kod

--- lib/product/components/appbar.dart
+++ lib/product/components/appbar.dart
@@ -1,28 +1,50 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:jista/views/main/vm/main_view_model.dart';
 
-AppBar appbar() {
-  return AppBar(
-    centerTitle: true,
-    title: const Text(
-      'Kategoriler',
-      style: TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w700),
-    ),
-    actions: [
-      Badge(
-        badgeColor: Colors.green,
-        animationType: BadgeAnimationType.fade,
-        position: const BadgePosition(start: 20, bottom: 25),
-        alignment: const Alignment(0, 0.3),
-        badgeContent: const Text('5'),
-        child: InkWell(
-          child: const Icon(Icons.add_shopping_cart_sharp, color: Colors.grey),
-          onTap: () {
-            // CARTTA TIKLANINCA
-          },
-        ),
+class MyAppBar {
+  static final MainViewModel _controller = Get.find<MainViewModel>();
+
+  static AppBar getAppBar() {
+    return AppBar(
+      centerTitle: true,
+      title: Obx(
+        () => Text(_controller.appbarTitle.value,
+            style: const TextStyle(fontFamily: 'Montserrat', fontWeight: FontWeight.w700)),
       ),
-      const SizedBox(width: 30),
-    ],
-  );
+      leading: _buildLeading(),
+      actions: [
+        Badge(
+          badgeColor: Colors.white,
+          animationType: BadgeAnimationType.fade,
+          position: const BadgePosition(start: 20, bottom: 25),
+          alignment: const Alignment(0, 0.3),
+          badgeContent: const Text(
+            '52',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+          ),
+          child: InkWell(
+            child: const Icon(Icons.add_shopping_cart_sharp, color: Colors.white),
+            onTap: () {
+              print('Carta tıklandı');
+            },
+          ),
+        ),
+        const SizedBox(width: 30),
+      ],
+    );
+  }
+
+  // APPBAR DA LEADİNGİ KAPATMAM GEREK CATEGORY SAYFASINDAN GİTTİĞİMDA APPBARDA DRAWER YERİNE GERİ BUTONU OLARAK LEADİNGE ICON ATAMAM VE O ICON İLE GERİ DÖNÜŞÜ SAĞLAMALIYIM.
+  static Widget? _buildLeading() {
+    return Obx(
+      () => _controller.isAppbarLeading.value
+          ? InkWell(
+              onTap: () => _controller.selectIndex.value = 0,
+              child: const Icon(Icons.arrow_back_ios_outlined),
+            )
+          : null,
+    );
+  }
 }
