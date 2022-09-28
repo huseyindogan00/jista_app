61ba12f5fc4406d9ee460bbef263c638c060e66c
HuseyinDOGAN <huseyindgn00@gmail.com>
2022-09-28 23:20:58 +0300

    getx uyarlama devam

---
+++ lib/product/models/base/base_model.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class BaseModel extends StatelessWidget {
+  const BaseModel({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
