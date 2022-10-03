e649a81ba17dad1b04d4bcb7f4d2bb6be64bb282
HuseyinDOGAN <huseyindgn00@gmail.com>
2022-10-04 01:22:19 +0300

    .

--- lib/views/cart/view/cart_details_view.dart
+++ lib/views/cart/view/cart_details_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:jista/data/constant/font/const_text_style.dart';
 import 'package:jista/product/models/product/product_model.dart';
 
 class CartDetailsView extends StatelessWidget {
@@ -14,37 +15,40 @@ class CartDetailsView extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         systemOverlayStyle: const SystemUiOverlayStyle(statusBarBrightness: Brightness.light),
-        title: Text(product.title),
+        title: Text(
+          product.type,
+        ),
       ),
-      body: Hero(
-        tag: product.id,
-        child: Expanded(
-          child: SizedBox(
-            child: Column(
+      body: Container(
+        margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        child: Column(
+          textDirection: TextDirection.ltr,
+          children: [
+            Text(
+              product.title,
+            ),
+            Container(
+                margin: const EdgeInsets.all(20),
+                child: const Image(image: AssetImage('assets/images/jandarma_logo.jpg'), fit: BoxFit.cover)),
+            Text(product.title),
+            OverflowBar(
               children: [
-                Center(child: Text(product.type)),
-                Container(
-                    margin: const EdgeInsets.all(10),
-                    child: const Image(image: AssetImage('assets/images/jandarma_logo.jpg'), fit: BoxFit.cover)),
-                Text(product.title),
-                Row(
-                  children: [
-                    Text(product.point.toString()),
-                    TextButton(
-                      onPressed: () {
-                        /****************************************/
-                        // SEPETE EKLEME YAPILACAK
-                      },
-                      child: Text(
-                        'EKLE',
-                        style: TextStyle(fontSize: 18, color: Colors.green.shade900),
-                      ),
-                    )
-                  ],
+                Text(
+                  product.point.toString(),
+                ),
+                TextButton(
+                  onPressed: () {
+                    /****************************************/
+                    // SEPETE EKLEME YAPILACAK
+                  },
+                  child: Text(
+                    'EKLE',
+                    style: TextStyle(fontSize: 18, color: Colors.green.shade900),
+                  ),
                 )
               ],
             ),
-          ),
+          ],
         ),
       ),
     );
--- lib/views/cart/view/cart_view.dart
+++ lib/views/cart/view/cart_view.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:jista/core/router/route_name.dart';
@@ -29,44 +31,59 @@ class _CartViewState extends State<CartView> {
     return _buildGridView(widget.productList!);
   }
 
-  GridView _buildGridView(List<ProductModel> producList) {
-    return GridView.builder(
-      itemCount: producList.length,
-      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemBuilder: (context, index) {
-        var product = producList[index];
-        return Hero(
-          tag: product.id,
-          child: InkWell(
-            child: Wrap(
-              spacing: 5,
-              children: [
-                Center(child: Text(product.type)),
-                Container(
-                    margin: const EdgeInsets.all(10),
-                    child: const Image(image: AssetImage('assets/images/jandarma_logo.jpg'), fit: BoxFit.cover)),
-                Text(product.title),
-                Row(
-                  children: [
-                    Text(product.point.toString()),
-                    TextButton(
-                      onPressed: () {
-                        /****************************************/
-                        // SEPETE EKLEME YAPILACAK
-                      },
-                      child: Text(
-                        'EKLE',
-                        style: TextStyle(fontSize: 18, color: Colors.green.shade900),
+  Widget _buildGridView(List<ProductModel> producList) {
+    return producList.isNotEmpty
+        ? GridView.builder(
+            itemCount: producList.length,
+            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemBuilder: (context, index) {
+              var product = producList[index];
+              return Container(
+                color: Colors.grey,
+                padding: const EdgeInsets.all(10),
+                margin: const EdgeInsets.all(10),
+                child: InkWell(
+                  onTap: () => Get.toNamed(RouteName.cartDetailsView, arguments: product),
+                  child: Column(
+                    children: [
+                      Expanded(
+                          child: Center(
+                              child: Text(
+                        product.type,
+                        style: TextStyle(fontSize: 10),
+                      ))),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.all(10),
+                          child: const Image(
+                            image: AssetImage('assets/images/jandarma_logo.jpg'),
+                            fit: BoxFit.cover,
+                          ),
+                        ),
                       ),
-                    )
-                  ],
-                )
-              ],
-            ),
-            onTap: () => Get.toNamed(RouteName.cartDetailsView, arguments: product),
-          ),
-        );
-      },
-    );
+                      Expanded(child: Text(product.title, style: TextStyle(fontSize: 10))),
+                      Row(
+                        children: [
+                          Expanded(child: Text(product.point.toString(), style: TextStyle(fontSize: 10))),
+                          Container(
+                            width: 60,
+                            height: 20,
+                            child: ElevatedButton(
+                              onPressed: () {
+                                /****************************************/
+                                // SEPETE EKLEME YAPILACAK
+                              },
+                              child: Text('EKLE', style: TextStyle(fontSize: 10, color: Colors.green.shade900)),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              );
+            },
+          )
+        : const Center(child: Text('Ürün Listesi Boş'));
   }
 }
