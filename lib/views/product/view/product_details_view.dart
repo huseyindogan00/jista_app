5fcac539464851b6dd07fe93baf6981871cbb97d
huseyindogan00 <huseyindgn00@gmail.com>
2022-10-20 16:29:51 +0300

    s

--- lib/views/product/view/product_details_view.dart
+++ lib/views/product/view/product_details_view.dart
@@ -12,43 +12,43 @@ class ProductDetailsView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     ProductModel product = productModel;
+    print('product sayfasında olan id-----------> ${product.id} ');
     return Scaffold(
       body: Center(
-        child: Hero(
-          tag: product.id,
-          child: Container(
-            color: Colors.grey,
-            margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
-            child: Column(
-              textDirection: TextDirection.ltr,
-              children: [
-                Text(product.title),
-                Expanded(
-                  child: Container(
-                      margin: const EdgeInsets.all(20),
-                      child: const Image(
-                          image: NetworkImage(
-                              'https://firebasestorage.googleapis.com/v0/b/jista-81374.appspot.com/o/hizmet_kiyafeti%2Fhizmet_kiyafeti.png?alt=media&token=ff60b28b-7be1-47d2-96da-89000721d5b7'),
-                          fit: BoxFit.cover)),
+        child: Container(
+          color: Colors.grey,
+          margin: const EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+          child: Column(
+            textDirection: TextDirection.ltr,
+            children: [
+              Text(product.title),
+              Expanded(
+                child: Container(
+                  margin: const EdgeInsets.all(20),
+                  child: const Image(
+                      image: NetworkImage(
+                          'https://firebasestorage.googleapis.com/v0/b/jista-81374.appspot.com/o/hizmet_kiyafeti%2Fhizmet_kiyafeti.png?alt=media&token=ff60b28b-7be1-47d2-96da-89000721d5b7'),
+                      fit: BoxFit.cover),
                 ),
-                Text(product.title),
-                OverflowBar(
-                  children: [
-                    Text(product.point.toString()),
-                    TextButton(
-                      onPressed: () {
-                        /****************************************/
-                        // SEPETE EKLEME YAPILACAK
-                      },
-                      child: Text(
-                        'EKLE',
-                        style: TextStyle(fontSize: 18, color: Colors.green.shade900),
-                      ),
-                    )
-                  ],
-                ),
-              ],
-            ),
+              ),
+              Text(product.title),
+              OverflowBar(
+                children: [
+                  Text(product.point.toString()),
+                  TextButton(
+                    onPressed: () {
+                      /****************************************/
+                      // SEPETE EKLEME YAPILACAK
+                    },
+                    child: Text(
+                      'EKLE',
+                      style:
+                          TextStyle(fontSize: 18, color: Colors.green.shade900),
+                    ),
+                  )
+                ],
+              ),
+            ],
           ),
         ),
       ),
